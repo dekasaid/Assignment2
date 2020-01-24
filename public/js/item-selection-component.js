ceaca36b8d5df4c93dee738e0fabccf83fadb850
Deka Said <said0060@algonquinlive.com>
2020-01-24 16:50:15 -0500

    some entities modfied, and added more assets

--- public/js/item-selection-component.js
+++ public/js/item-selection-component.js
@@ -13,21 +13,13 @@ init: function()
             //object3D = three.js(rendering engine) 3D element
           
             
-           // Context_AF.el.object3D.scale.set(0.020,0.020,0.020);
-           Context_AF.el.object3D.rotation.set(
-            THREE.Math.degToRad(15),
-            THREE.Math.degToRad(30),
-            THREE.Math.degToRad(90)
-          );
+           Context_AF.el.object3D.scale.set(0.025, 0.025, 0.025);
+           //Context_AF.el.object3D.rotate.set();
         });
 
         Context_AF.el.addEventListener('mouseleave', function(event)
         {
-            Context_AF.el.object3D.rotation.set(
-                THREE.Math.degToRad(0),
-                THREE.Math.degToRad(0),
-                THREE.Math.degToRad(90)
-            );
+            Context_AF.el.object3D.scale.set(0.015, 0.015, 0.015);
 
         });
 
